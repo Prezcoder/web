5a43a58a14b9943eaee7c61766dabdacd66efe9b
Prezcoder <fbouchar@student.42quebec.com>
2024-02-06 10:15:27 -0500

    MAJ

---
+++ web/views.py
@@ -0,0 +1,8 @@
+
+from django.http import HttpResponse
+
+def game(request):
+	return HttpResponse("Hello, world. You're at the game index.")
+
+def home(request):
+	return HttpResponse("Hello, world. You're at the home index.")
