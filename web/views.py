e755a523e2c74cc3e006148bed68945aa984c1ce
Prezcoder <fbouchar@student.42quebec.com>
2024-02-07 09:27:25 -0500

    MAJ game.html setup

--- web/views.py
+++ web/views.py
@@ -1,10 +1,32 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
+data = {
+	'game': [
+		{
+			'id': 5,
+			'title': 'Pong',
+			'year': 2024
+		},
+		{
+			'id': 6,
+			'title': 'Ping',
+			'year': 2025
+		},
+		{
+			'id': 7,
+			'title': 'Pouf',
+			'year': 2026
+		}
+
+	]
+
+}
 
 def game(request):
-	return HttpResponse("Hello, world. You're at the game index.")
+	return render(request, 'game/game.html', data)
 
 def home(request):
 	return HttpResponse("Hello, world. You're at the home index.")
 
-//test
